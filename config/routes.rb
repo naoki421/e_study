43e8bc1e26e51a0a40841a76d6080f7630ea3bb9
naoki421 <nyh1218421@gmail.com>
2022-06-02 08:36:47 +0000

    [Add]deviseでユーザー認証機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  #管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  #ユーザー用
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  root to: "public/homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
