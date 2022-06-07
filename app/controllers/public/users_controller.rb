4c0f04ddd77a7b2abbad930aa50abda4654b414b
naoki421 <nyh1218421@gmail.com>
2022-06-07 08:08:03 +0000

    [Add]マイページ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,28 @@
 class Public::UsersController < ApplicationController
-  
-  def index
-    
+
+  def show
+    @user = User.find(params[:id])
+    @post_study_method = @user.post_study_methods
   end
-  
+
+  def index
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :school_year, :profile_image)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_one_attached :profile_image
+
   validates :name, presence: true
   validates :school_year, presence: true, numericality: { in: 1..7 }
 
@@ -18,6 +20,14 @@ class User < ApplicationRecord
       user.password = SecureRandom.urlsafe_base64
       user.email = "guest@example.com"
     end
+  end
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
